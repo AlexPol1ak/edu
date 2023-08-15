2adf3e4b51dbf80ebffb77954e270012e21b2d9e
Alex <zarjrw1994@gmail.com>
2023-08-15 20:07:09 +0300

    An abstract model of content types has been created. Models for storing course content are created.

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -41,3 +43,48 @@ class Module(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     # order = OrderField(blank=True, for_fields=['course'])
+
+
+class Content(models.Model):
+    """Модель для связывания типа контента модуля с модулем."""
+
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    """Абстрактная модель контента."""
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    """Модель текстового контента."""
+    content = models.TextField()
+
+
+class File(ItemBase):
+    """Модель файлов."""
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    """Модель изображений."""
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    """Модель видео."""
+    url = models.URLField()
