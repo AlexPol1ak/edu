ad22ca350f6eb1bb05b45600c93b5d27ebe94775
Alex <zarjrw1994@gmail.com>
2023-08-16 18:34:38 +0300

    A basic authentication system has been added.

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from educa import settings
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
 
