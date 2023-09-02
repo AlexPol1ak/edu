d1339b75b825d83ee6a813298e6165c12402ca7e
Alex <zarjrw1994@gmail.com>
2023-09-03 01:29:58 +0300

    Created routes, views, templates to display student's course list and detailed course display. Default redirection is defined in the settings.

--- educa/students/views.py
+++ educa/students/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
+from courses.models import Course
 from students.froms import CourseEnrollForm
 
 
@@ -17,7 +19,7 @@ class StudentRegistrationView(CreateView):
     def form_valid(self, form):
         result = super().form_valid(form)
         cd = form.cleaned_data
-        user = authenticate(username=cd['username'], password=cd['password'])
+        user = authenticate(username=cd['username'], password=cd['password1'])
         login(self.request, user)
         return result
 
@@ -34,3 +36,35 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    """Представление для просмотра курсов на которые зачислены студенты."""
+
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qr = super().get_queryset()
+        return qr.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    """Представление для детального отображения курса студента."""
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qr = super().get_queryset()
+        return qr.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        course = self.get_object()
+
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            context['module'] = course.modules.all()
+
+        return context
