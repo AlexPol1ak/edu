3922e4d2e2333901bf4758e375d3db57589f52e7
Alex <zarjrw1994@gmail.com>
2023-09-05 18:35:18 +0300

    A route has been created, a view to enroll the user in the course.

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -6,4 +6,5 @@ app_name = 'courses'
 urlpatterns = [
     path('subjects/', views.SubjectApiListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', views.SubjectApiDetailView.as_view(), name='subject_detail'),
+    path('courses/<pk>/enroll/', views.CourseEnrollApiView.as_view(), name='course_enroll')
 ]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class SubjectApiListView(generics.ListAPIView):
     """Представление для отображения всех курсов."""
@@ -11,3 +17,14 @@ class SubjectApiDetailView(generics.RetrieveAPIView):
     """Представление для отображения курса детально."""
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollApiView(APIView):
+    """Представление для записи пользователей на курсы."""
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
