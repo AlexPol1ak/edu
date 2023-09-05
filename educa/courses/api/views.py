f25e9c6d5ceabd43596c914548f7c7cecc0d36fa
Alex <zarjrw1994@gmail.com>
2023-09-05 19:03:13 +0300

    CourseEnrollApiView view has been changed.

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,12 +21,30 @@ class SubjectApiDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
+# class CourseEnrollApiView(APIView):
+#     """Представление для записи пользователей на курсы."""
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
+
 class CourseEnrollApiView(APIView):
     """Представление для записи пользователей на курсы."""
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
 
+class CourseApiViewSet(viewsets.ReadOnlyModelViewSet):
+    """Набор представлений для чтения курсов."""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
